2b2a8db5d835d5624de0b6aec465d0f2abf6d371
Aliaksei Rudak <arudak@ebsco.com>
2024-03-06 16:25:31 +0600

    feat: add album template

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { TracksModule } from './track/track.module';
 import { ArtistsModule } from './artists/artists.module';
 import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [UserModule, TracksModule, ArtistsModule, ArtistModule],
+  imports: [UserModule, TracksModule, ArtistsModule, ArtistModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })

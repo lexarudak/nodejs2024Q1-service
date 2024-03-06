e6098741ef8c386c92aee40f44a6ce677bfb1efa
Aliaksei Rudak <arudak@ebsco.com>
2024-03-06 16:28:10 +0600

    fix: remofe old ver

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TracksModule } from './track/track.module';
-import { ArtistsModule } from './artists/artists.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [UserModule, TracksModule, ArtistsModule, ArtistModule, AlbumModule],
+  imports: [UserModule, TracksModule, ArtistModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })

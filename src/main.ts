343505f500cb55ebff7285a1db7c3acd790394a8
Aliaksei Rudak <lexarudak2@gmail.com>
2024-03-07 08:22:36 +0500

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
 
 config();
 const port = process.env.PORT || 4000;
@@ -11,14 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('REST Service')
-    .setDescription('The REST Service API description')
-    .setVersion('1.0')
-    .addTag('rest-service')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  const yamlDocument = YAML.load('./doc/api.yaml');
+  SwaggerModule.setup('docs', app, yamlDocument);
   await app.listen(port);
 }
 bootstrap();

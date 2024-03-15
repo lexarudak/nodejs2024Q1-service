2bc1ca4686edb6ecbe6258190a1d9a31ecf41dfa
Aliaksei Rudak <arudak@ebsco.com>
2024-03-15 17:29:45 +0500

    feat: start db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,25 @@ import { TracksModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UserModule, TracksModule, ArtistModule, AlbumModule, FavsModule],
+  imports: [
+    UserModule,
+    TracksModule,
+    ArtistModule,
+    AlbumModule,
+    FavsModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'rs-node',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
